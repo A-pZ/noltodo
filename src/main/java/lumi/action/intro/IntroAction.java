3dc73f3adc1274ec89cfa2c164afc0057ac38c6f
A-pZ <a-pz@h4.dion.ne.jp>
2015-02-26 17:17:36 +0900

    導入ページの作成 #97

---
+++ src/main/java/lumi/action/intro/IntroAction.java
@@ -0,0 +1,42 @@
+package lumi.action.intro;
+
+import lombok.extern.slf4j.Slf4j;
+import lumi.action.LumiActionSupport;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.Results;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * 紹介画面表示Action。
+ *
+ * @author A-pZ ( Serendipity 3 ./ as sundome goes by. )
+ */
+@Namespace("/intro")
+@ParentPackage("lumi-default")
+@Results({
+	// location属性に指定したhtmlファイル名は、/WEB-INF/content 以下からの相対パス。
+	@Result(name = ActionSupport.SUCCESS, location = "/intro/intro" , type="thymeleaf-spring"),
+})
+@Controller
+@Scope("prototype")
+@Slf4j
+public class IntroAction extends LumiActionSupport {
+
+	/**
+	 * デフォルトアクション。
+	 */
+	@Action("")
+	public String start() throws Exception {
+
+		// Result値。ActionSupportの定数値を返すか、別途定義した値を返すこと。
+		// この値は@Resultで指定したname値となる。
+		return SUCCESS;
+	}
+}
