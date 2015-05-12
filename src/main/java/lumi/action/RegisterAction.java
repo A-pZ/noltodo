f8a6672832b15692233c4b0b90cba2798153ed9a
A-pZ <a-pz@h4.dion.ne.jp>
2015-05-12 14:54:06 +0900

    登録済みタグからも登録タグを選択できるようにする #104

--- src/main/java/lumi/action/RegisterAction.java
+++ src/main/java/lumi/action/RegisterAction.java
@@ -1,10 +1,13 @@
 package lumi.action;
 
+import java.util.List;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.val;
 import lombok.extern.slf4j.Slf4j;
 import lumi.service.RegisterService;
+import lumi.service.TagService;
 import lumi.vo.RegisterVO;
 
 import org.apache.struts2.convention.annotation.Action;
@@ -60,6 +63,9 @@ public class RegisterAction extends LumiActionSupport {
 		// 登録後、再検索した値を取得
 		vo = service.getRegisterVO();
 
+		// 1.3.0 タグ一覧の取得
+		tagList = tagService.getEffectiveTagList();
+
 		// Result値。ActionSupportの定数値を返すか、別途定義した値を返すこと。
 		// この値は@Resultで指定したname値となる。
 		return ( result ? SUCCESS : ERROR );
@@ -73,6 +79,13 @@ public class RegisterAction extends LumiActionSupport {
 	@Getter @Setter
 	private RegisterService service;
 
+	@Blocked
+	@Autowired
+	private TagService tagService;
+
 	@Getter @Setter
 	private RegisterVO vo;
+
+	@Getter @Setter
+	private List<String> tagList;
 }
